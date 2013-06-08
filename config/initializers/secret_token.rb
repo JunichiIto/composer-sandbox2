d482ca265549c8c601c2c28c025a877b176462c5
Junichi Ito <jit@sonicgarden.jp>
2013-06-08 16:21:04 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComposerSandbox2::Application.config.secret_token = 'e0477211dde69aee473d04f9fbba47e91e38685cb6e870ed653e35cba6fb562ad0dbddaf9aa241a21c6e636080033c03bf91b56722c3fcb015ce34f18ef25326'
